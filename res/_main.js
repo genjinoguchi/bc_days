9860cbada1a4d2ab845c36a66bbc6a69fa06a37e
Genji Noguchi <genji.noguchi@gmail.com>
2014-07-13 09:27:11 -0400

    Added some more resource files.
    Some more resource files, such as the flat buttons and icons, were
    added to the page.

--- res/_main.js
+++ res/_main.js
@@ -1,1 +1,11 @@
-/* Main js */
+
+
+//Constants
+var document-width = $(document).width();
+var document-height= $(document).height();
+
+//A dictionary of projects
+var projects = {}
+
+
+//Init the 
