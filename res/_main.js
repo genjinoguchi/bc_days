08e5f5577b23090f0650d321fd516c2950caf44c
Genji Noguchi <genji.noguchi@gmail.com>
2014-07-13 19:25:42 -0400

    Design changes
    Change in plans. Switching to a side pane. (sorry html5)

--- res/_main.js
+++ res/_main.js
@@ -6,4 +6,27 @@
 var projects = {}
 
 
-//Init the 
+//Init the projects.
+
+
+
+
+
+//jQuery Actions
+
+$(document).ready(function(){
+	intro();
+})
+
+//Functions
+
+function intro(){
+	$(".menu-heading").animate({
+		opacity: '1',
+		top: '-10px'
+	}, "slow");
+
+
+
+
+}
