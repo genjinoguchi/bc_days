331ad9e2286a701655e3b46b560452caae35c777
Genji Noguchi <genji.noguchi@gmail.com>
2014-07-14 00:55:38 -0400

    Stable first tab!
    Stable stuff in there yesh

--- res/_main.js
+++ res/_main.js
@@ -1,6 +1,7 @@
 //main js script
 
 //Constants
+var menuToggled = false;
 
 //A dictionary of projects
 var projects = {}
@@ -16,17 +17,34 @@ var projects = {}
 
 $(document).ready(function(){
 	intro();
+
+	$("#option-button").click(function(){
+		toggleMenu();
+	});
 })
 
 //Functions
 
 function intro(){
-	$(".menu-heading").animate({
+	$("#heading-main").velocity({
 		opacity: '1',
-		top: '-10px'
+		top: '250px'
 	}, "slow");
+	
+	$("#pointer").delay(1000).show("slow");
+}
 
-
-
-
+function toggleMenu(){
+	if(!menuToggled){
+		//Change this to javascript animations later on.
+		document.getElementById("side-menu-bar").style.left = "0px";
+		document.getElementById("intro-page").style.right = "-400px";
+		document.getElementById("option-button").style.left = "30%";
+		$("#pointer").delay(1000).hide("slow");
+	}else{
+		document.getElementById("side-menu-bar").style.left = "-400px";
+		document.getElementById("intro-page").style.right = "0px";
+		document.getElementById("option-button").style.left = "2%";
+	}
+	menuToggled = !menuToggled;
 }
