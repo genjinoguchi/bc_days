da21120e0edae98069b3162ec92ceac0ecc207d8
Genji Noguchi <genji.noguchi@gmail.com>
2015-06-28 13:39:28 -0400

    Miscellaneous things. Added images, minor grid fixes.

--- res/js/main.js
+++ res/js/main.js
@@ -1,2 +1,21 @@
 
+// Animation Functions
 
+function showMenuBar() {
+
+}
+function hideMenuBar() {
+
+}
+
+
+
+
+
+document.ready(function() {
+	// Load button callbacks with jQuery
+	
+	$("#mobile-navbar-menu-button").click(function() {
+
+	})
+})
